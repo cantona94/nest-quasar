51866bea47dbead9db234eb2d7ab180becbf4ced
Vladimir Staninov <52317262+cantona94@users.noreply.github.com>
2023-08-22 18:19:45 +0500

    add form with user information

--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -1,21 +1,62 @@
 <template>
-  <q-page class="row items-center justify-center">
-    <h1>{{ prop }} </h1>
+  <q-page class="column items-center justify-center">
+    <h6 class="text-h6 q-my-none text-weight-regular">Your information</h6>
+    <q-form v-if="isAuth" class="q-gutter-md" style="width: 50vh">
+      <q-input label="Name" class="input" v-model="userInformation.name" />
+      <q-input label="Phone" class="input" v-model="userInformation.phone" />
+      <q-input
+        label="Address"
+        class="input"
+        v-model="userInformation.address"
+      />
+      <q-input
+        label="Adout me"
+        class="input"
+        v-model="userInformation.adoutMe"
+      />
+      <div>
+        <q-btn
+          class="full-width"
+          color="primary"
+          label="change information"
+          type="submit"
+          rounded
+        ></q-btn>
+      </div>
+    </q-form>
+    <h1 v-else>Home</h1>
   </q-page>
 </template>
 
 <script lang="ts">
-
-import { useStore } from 'src/store';
 export default {
   name: 'HomePage',
-  setup() {
-    const $store = useStore();
-    const prop = $store.state.example.isAuth
-
+  data() {
     return {
-      prop,
+      isAuth: this.$store.state.example.isAuth,
+      userInformation: {
+        name: '',
+        phone: '',
+        address: '',
+        adoutMe: '',
+      },
     };
+  },
+  methods: {
+    checkAuth() {
+      this.isAuth = this.$store.state.example.isAuth;
+    },
+  },
+  watch: {
+    '$store.state.example.isAuth': {
+      handler() {
+        this.checkAuth();
+      },
+      immediate: true,
+    },
+  },
+  mounted() {
+    this.checkAuth();
   },
 };
 </script>
